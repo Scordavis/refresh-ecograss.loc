36dfbdf683de9972d03d4c623f0ad22d075a590e
Scordavis <scordavis@gmail.com>
2018-03-23 20:51:05 +0200

    js jquery.fancybox and jquery.maskedinput

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,9 @@ gulp.task('less', function() {
 gulp.task('scripts', function(){
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/libs/respond/dist/respond.min.js',
+		// 'app/libs/respond/dist/respond.min.js',
+		'app/libs/respond/dist/jquery.fancybox.js',
+		'app/libs/respond/dist/jquery.maskedinput.js',
 		])
 	.pipe(concat('libs.min.js'))
 	.pipe(uglify())
